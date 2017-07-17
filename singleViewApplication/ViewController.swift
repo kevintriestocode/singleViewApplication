1f882b5d6e7a6d7df9fad5a77c7c7ee78af0922f
kevintriestocode <kwangsan@gmail.com>
2017-07-17 14:14:25 -0400

    Commit app from work

--- singleViewApplication/ViewController.swift
+++ singleViewApplication/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var label: UILabel!
+   
+    @IBAction func buttonPress(_ sender: Any) {
+        label.text = "Changed"
+    }
 
+    
+    
 }
 
